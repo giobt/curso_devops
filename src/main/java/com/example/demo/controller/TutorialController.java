6e0da64edc05661ae1fc056f9ab03dcd31efc7e1
Giorgio Balconi <giobt@hotmail.com>
2022-04-01 22:40:04 +0200

    Added new api call

--- src/main/java/com/example/demo/controller/TutorialController.java
+++ src/main/java/com/example/demo/controller/TutorialController.java
@@ -52,6 +52,16 @@ public class TutorialController {
         }
     }
 
+    @GetMapping("/tutorials/description/{id}")
+    public ResponseEntity<Tutorial> getTutorialById2(@PathVariable("id") long id) {
+        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+        if (tutorialData.isPresent()) {
+            return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
     @PostMapping("/tutorials")
     public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
         try {
